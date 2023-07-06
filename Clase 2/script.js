296fbeff0798bf27d2194cfc0a1dd6be6bcd855a
Joaquín Julián Cortés <joaquinjuliancortesit@gmail.com>
2023-07-05 22:43:46 -0300

    My second class at coderhouse'

---
+++ Clase 2/script.js
@@ -0,0 +1,25 @@
+// let numberOne = 10;
+// let numberTwo = 12;
+
+// let sum = numberOne + numberTwo;
+// let difference = numberOne - numberTwo;
+// let division = numberOne / numberTwo;
+// let multiplication = numberOne / numberTwo;
+
+// console.log(numberOne + " plus " + numberTwo + " is equal to " + sum);
+
+// let nombre = prompt("What's your name?");
+// console.log("Hi " + nombre);
+
+// alert("Hi, " + nombre + " you are a great person");
+// alert("The variable is a " + typeof nombre + " data type");
+
+// 1. Crea un script en JS que le solicite al usuario ingresar mínimo 1 dato y luego, mediante JavaScript, realiza operaciones sobre los mismos
+
+let name = prompt("What's your name? ");
+let age = prompt("How old are you? ");
+let salary = parseInt(prompt("How much money do you earn per day? "));
+
+console.log("Your name is: " + this.name);
+console.log("You are " + age + " years old");
+console.log("Your monthly income is " + salary * 30);
